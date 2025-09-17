7a73fa9aea86af890ce053d3ab5d8fae6ab604b7
Aproglobal <aproglobal@kukjeint.com>
2025-09-17 16:19:52 +0900

    Create FleetSection.tsx

---
+++ src/components/FleetSection.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function FleetSection() {
+  return (
+    <section style={{ padding: 24 }}>
+      <h2>Fleet</h2>
+      <p>Coming soon.</p>
+    </section>
+  );
+}
