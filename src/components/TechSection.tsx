87aab46e64ba8f948954a147aee17df291432161
Aproglobal <aproglobal@kukjeint.com>
2025-09-17 16:19:31 +0900

    Create TechSection.tsx

---
+++ src/components/TechSection.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function TechSection() {
+  return (
+    <section style={{ padding: 24 }}>
+      <h2>Technology</h2>
+      <p>Coming soon.</p>
+    </section>
+  );
+}
