b5a64bbb8b58b8ba26d7b843dc1379c37718d742
Aproglobal <aproglobal@kukjeint.com>
2025-09-24 11:12:37 +0900

    Update ModelDetail.tsx

--- src/components/ModelDetail.tsx
+++ src/components/ModelDetail.tsx
@@ -166,7 +166,7 @@ export default function ModelDetail() {
 
               <div className="mt-6 flex flex-wrap gap-3">
                 <button
-                  onClick={() => openLead(`Model Detail ${model.code}`)}
+                  onClick={() => openLead(`Model ${model.code}`, { modelCode: model.code })}
                   className="px-5 py-3 rounded-full bg-black text-white font-semibold dark:bg-white dark:text-black"
                 >
                   Talk to Sales
