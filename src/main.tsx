c2e8d0c3cdc965f65375efcf73fa376751615662
Aproglobal <aproglobal@kukjeint.com>
2025-10-14 16:43:27 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,21 @@
-// src/main.tsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './pages/App';
-import { initAnalytics, setupSpaPageviews } from './services/analytics';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './pages/App'
+import { initAnalytics, setupSpaPageviews } from './services/analytics'
+import { loadRecaptcha } from './lib/recaptcha'
 
-initAnalytics(import.meta.env.VITE_GA_MEASUREMENT_ID).then(() => {
-  setupSpaPageviews();
-});
+// ✅ 앱 마운트 전에 reCAPTCHA 스크립트 미리 로드
+const siteKey = import.meta.env.VITE_RECAPTCHA_SITE_KEY as string | undefined;
+if (siteKey) {
+  // 실패해도 앱 구동은 계속되도록 catch
+  loadRecaptcha(siteKey).catch(() => {});
+}
+
+initAnalytics().then(() => setupSpaPageviews())
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-);
+)
