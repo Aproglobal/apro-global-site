572d26a22a620e128b2ac27575205e02fc5969b0
Aproglobal <aproglobal@kukjeint.com>
2025-10-14 16:25:34 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './pages/App'
-import { initAnalytics, setupSpaPageviews } from './services/analytics'
+// src/main.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './pages/App';
+import { initAnalytics, setupSpaPageviews } from './services/analytics';
 
-initAnalytics().then(() => setupSpaPageviews())
+initAnalytics(import.meta.env.VITE_GA_MEASUREMENT_ID).then(() => {
+  setupSpaPageviews();
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
