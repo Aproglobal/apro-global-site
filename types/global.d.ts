128d45672fdb9a99ca70d174a01bb78309d76e6d
Aproglobal <aproglobal@kukjeint.com>
2025-10-15 11:06:27 +0900

    Update global.d.ts

--- types/global.d.ts
+++ types/global.d.ts
@@ -4,6 +4,7 @@ export {};
 declare global {
   interface Window {
     dataLayer?: any[];
-    gtag?: (...args: any[]) => void; // 전역 gtag는 '옵셔널'만 선언 (충돌 방지)
+    gtag?: (...args: any[]) => void;
+    __GA_ID__?: string;
   }
 }
