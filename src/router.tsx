61dfb38e9004326c0b9684730bb76e87c8b20485
Aproglobal <aproglobal@kukjeint.com>
2025-11-06 15:39:14 +0900

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,11 @@
 // src/router.tsx
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
-// 홈/공통
+// pages
 import Home from "./pages/home/Home";
 import Support from "./pages/support/Support";
 import Contact from "./pages/contact/Contact";
 
-// 모델
 import ModelsIndex from "./pages/models/ModelsIndex";
 import ModelDetail from "./pages/models/ModelDetail";
 import ModelsCompare from "./pages/models/ModelsCompare";
@@ -15,9 +14,9 @@ import ModelsTechnology from "./pages/models/ModelsTechnology";
 import ModelsService from "./pages/models/ModelsService";
 import ModelsCases from "./pages/models/ModelsCases";
 
-// 회사/파트너
 import CompanyAbout from "./pages/company/CompanyAbout";
 import CompanyResources from "./pages/company/CompanyResources";
+
 import Partners from "./pages/partners/Partners";
 import PartnersApply from "./pages/partners/PartnersApply";
 
@@ -26,19 +25,20 @@ import NotFound from "./pages/NotFound";
 export default function Router() {
   return (
     <Routes>
+      {/* ✅ 메인(Home) */}
       <Route path="/" element={<Home />} />
 
-      {/* Models */}
+      {/* Models 아래 모든 서브 섹션 일원화 */}
       <Route path="/models" element={<ModelsIndex />} />
       <Route path="/models/compare" element={<ModelsCompare />} />
       <Route path="/models/configurator" element={<ModelConfigurator />} />
-      <Route path="/models/technology" element={<ModelsTechnology />} />   {/* ⬅️ 이동 */}
-      <Route path="/models/service" element={<ModelsService />} />         {/* ⬅️ 이동 */}
-      <Route path="/models/cases" element={<ModelsCases />} />             {/* ⬅️ 이동 */}
+      <Route path="/models/technology" element={<ModelsTechnology />} />
+      <Route path="/models/service" element={<ModelsService />} />
+      <Route path="/models/cases" element={<ModelsCases />} />
       <Route path="/models/:slug" element={<ModelDetail />} />
 
       {/* Company */}
-      <Route path="/company/about" element={<CompanyAbout />} />
+      <Route path="/company" element={<CompanyAbout />} />
       <Route path="/company/resources" element={<CompanyResources />} />
 
       {/* Partners */}
@@ -49,11 +49,10 @@ export default function Router() {
       <Route path="/support" element={<Support />} />
       <Route path="/contact" element={<Contact />} />
 
-      {/* Legacy redirects */}
-      <Route path="/company/technology" element={<Navigate to="/models/technology" replace />} />
-      <Route path="/company/service" element={<Navigate to="/models/service" replace />} />
-      <Route path="/partners/cases" element={<Navigate to="/models/cases" replace />} />
+      {/* 레거시 리다이렉트 (있다면) */}
+      <Route path="/home" element={<Navigate to="/" replace />} />
 
+      {/* 404 */}
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
