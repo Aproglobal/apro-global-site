fdff7033c90cb9d43f18f6acd1038c3f11ba0e97
Aproglobal <aproglobal@kukjeint.com>
2025-11-06 16:08:58 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,22 @@
-module.exports = {
-  darkMode: 'class', // ← 반드시 class 모드
-  content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
-  theme: { extend: {} },
-  plugins: [],
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: [
+    './index.html',
+    './src/**/*.{ts,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: '#fff1f3',
+          100: '#ffe4e8',
+          500: '#f05676',
+          600: '#e24464',
+          700: '#c92a4b'
+        }
+      }
+    }
+  },
+  plugins: []
 };
