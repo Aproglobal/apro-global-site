945bea9dac9a387988ae154f32e7556d49eddb40
Aproglobal <aproglobal@kukjeint.com>
2025-11-06 15:15:41 +0900

    Create ModelsCompare.tsx

---
+++ src/pages/models/ModelsCompare.tsx
@@ -0,0 +1,10 @@
+import CompareTable from "../../components/CompareTable";
+
+export default function ModelsCompare() {
+  return (
+    <main className="mx-auto max-w-6xl px-4 md:px-6 py-10">
+      <h1 className="text-3xl font-extrabold tracking-tight mb-6">Compare Models</h1>
+      <CompareTable />
+    </main>
+  );
+}
