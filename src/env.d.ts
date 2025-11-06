7c8917082648e0c3c959919556c8eeca41ca4a6d
Aproglobal <aproglobal@kukjeint.com>
2025-11-06 13:52:59 +0900

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,7 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_SALES_EMAIL?: string;
   readonly VITE_GA_MEASUREMENT_ID?: string;
-  readonly VITE_RECAPTCHA_SITE_KEY?: string; // ⬅️ 추가
+  readonly VITE_RECAPTCHA_SITE_KEY?: string;
 }
 
 interface ImportMeta {
