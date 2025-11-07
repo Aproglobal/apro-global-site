08eb8e16e513a4286f5331eaa9edf3d6283d199b
Aproglobal <aproglobal@kukjeint.com>
2025-11-07 19:43:36 +0900

    Create runtime-config.js

---
+++ public/runtime-config.js
@@ -0,0 +1,4 @@
+// public/runtime-config.js
+window.RUNTIME_CONFIG = Object.assign(window.RUNTIME_CONFIG || {}, {
+  // VITE_GA_MEASUREMENT_ID: "G-XXXXXXXX", // fill if you want a runtime override
+});
