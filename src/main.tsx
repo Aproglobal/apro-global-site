05f3d9734a556f66d3bd0cd1a911d6f8ff2b5358
Aproglobal <aproglobal@kukjeint.com>
2025-11-08 15:08:37 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './pages/App'
-import { initAnalytics, setupSpaPageviews } from './services/analytics'
-import { loadRecaptcha } from './lib/recaptcha'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./pages/App";
+import { initAnalytics, setupSpaPageviews } from "./services/analytics";
+import { loadRecaptcha } from "./lib/recaptcha";
 
-// ✅ reCAPTCHA preload
+// reCAPTCHA preload (safe if not set)
 const siteKey = import.meta.env.VITE_RECAPTCHA_SITE_KEY as string | undefined;
 if (siteKey) {
+  // your helper expects a key argument
   loadRecaptcha(siteKey).catch(() => {});
 }
 
-initAnalytics().then(() => setupSpaPageviews());
+// GA init is synchronous in our implementation
+initAnalytics();
+setupSpaPageviews();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
